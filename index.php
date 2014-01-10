600f364909e3b6b47ec136f444e287802dc05883
root <root@lorinv-HP-Pavilion-g6-Notebook-PC.(none)>
2014-01-09 23:36:08 -0800

    basic website

--- index.php
+++ index.php
@@ -3,10 +3,62 @@
 <title> Home </title>
 </head>
 	<body style="background-color:black; font-family:garamond;">
-		<main style="background-color:gray; height:900px; width: 800px; margin:0 auto;">
-			<img src="wsuLogo.jpg">
-			
-			<hr style="color:black">
+		<main style="background-color:gray; height:1250px; width: 800px; margin:0 auto;">
+			<logo style="font-family:garamond; font-size:20px;"> SI </logo>
+			<img src="city.jpg" style="width:800px; height:300px; border-top-style:solid; border-bottom-style:solid;
+														 border-width:2px;">
+			<div style="padding-left:100px; padding-right:100px;">
+                        	<h3> Simple Intelligence </h3>
+				<div style="float:left; width:285px; font-size:12px;">
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+				has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+				took a galley of type and scrambled it to make a type specimen book. It has survived not only
+				five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+				It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+				passages, and more recently with desktop publishing software like Aldus PageMaker including
+				versions of Lorem Ipsum.
+				</div>
+				<div style="float:right; width:285px; font-size:12px;">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
+                                has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
+                                took a galley of type and scrambled it to make a type specimen book. It has survived not only 
+                                five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum 
+                                passages, and more recently with desktop publishing software like Aldus PageMaker including 
+                                versions of Lorem Ipsum.
+                                </div>
+				<br><br>
+				<h4 style="padding-top:180px;">The Team<h4>
+				<div style="float:left; width:285px; font-size:12px;">
+					<img src="lorin.jpg" style="width:200px; height:200px; border-style:solid; border-width:2px;">
+					<br>
+					Lorin Vandegrift <br>
+					Software Developer & Team Manager <br>
+					lorin.vandegrift@email.wsu.edu <br>
+					<br><br>
+					<img src="dustin.jpg" style="width:200px; height:200px; border-style:solid; border-width:2px;">
+                                        <br>
+                                        Dustin Crossman <br>
+                                        Lead Software Developer <br>
+                                        dustin.crossman@gmail.com <br>
+
+				</div>
+				<div style="float:right; width:285px; font-size:12px;">
+                                        <img src="alan.jpg" style="width:200px; height:200px; border-style:solid; border-width:2px;">
+                                        <br>
+                                        Alan Vandegrift <br>
+                                        Mechanical Engineer <br>
+                                        alvandegrift@hotmail.com <br>
+                                	<br><br>
+					<img src="cameron.jpg" style="width:200px; height:200px; border-style:solid; border-width:2px;">
+                                        <br>
+                                        Cameron Mehl <br>
+                                        Electrical Engineer <br>
+                                        me@cameronmehl.com <br>
+				</div>
+
+                        </div>
+
 		</main>
 	</body>
 </html>
