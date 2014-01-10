367c7bc03cf4155509f36efa3c189a26da855d20
Lorin <lt_vandegrift@live.com>
2014-01-09 18:34:15 -0800

    Added index and logos

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title> Home </title>
+</head>
+	<body style="background-color:black; font-family:garamond;">
+		<main style="background-color:gray; height:900px; width: 800px; margin:0 auto;">
+			<img src="wsuLogo.jpg">
+			
+			<hr style="color:black">
+		</main>
+	</body>
+</html>
