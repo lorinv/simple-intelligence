97a26d14f3c0c9b64aee41adbb005cec3632acff
root <root@lorinv-HP-Pavilion-g6-Notebook-PC.(none)>
2014-01-10 10:10:09 -0800

    add views

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+$views = 0;
+$visitors_file = "views.txt";
+
+// Load up the persisted value from the file and update $views
+if (file_exists($visitors_file))
+{
+    $views = (int)file_get_contents($visitors_file); 
+}
+
+// Increment the views counter since a new visitor has loaded the page
+$views++;
+
+// Save the contents of this variable back into the file for next time
+file_put_contents($visitors_file, $views);
+?>
 <html>
 <head>
 <title> Home </title>
@@ -62,3 +78,5 @@
 		</main>
 	</body>
 </html>
+
+<h7 style="color:white"> <?php echo $views;?></h7>
