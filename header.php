5c68c41e6ea11d98ffb986f28035061f07abd110
Lorin <lt_vandegrift@live.com>
2014-02-27 02:12:13 -0800

    Added correct link to pictures on top

--- header.php
+++ header.php
@@ -1,7 +1,8 @@
 <?php
 	echo '<div id="Top" style="background-color:black; margin-top:20px; width:800px; padding-top:10px; padding-bottom:15px; padding-left:10px; ">
 		<center>
-			<img style="height:60px;"src="images/Logo_White.PNG"><img style="padding-right:5px; padding-left:5px; height:43px; padding-bottom:10px;"src="images/and.PNG"><img style="height:60px;"src="images/wsu_logo.PNG">
+			<img style="height:60px;"src="images/Logo_White.png"><img style="padding-right:5px; padding-left:5px;
+			 height:43px; padding-bottom:10px;"src="images/and.png"><img style="height:60px;"src="images/wsu_logo.png">
 		<center>
 	</div>
 	<div id="navBar" style="width:800px;">
